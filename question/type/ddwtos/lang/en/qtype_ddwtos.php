65332234306ffce3b85793fd96dcba36744a1ecd
Tadeus <tadeuslima@outlook.com>
2018-03-04 11:22:46 -0300

    Adicionando strings que serão usadas no formulário de criação/edição da questão

--- question/type/ddwtos/lang/en/qtype_ddwtos.php
+++ question/type/ddwtos/lang/en/qtype_ddwtos.php
@@ -34,3 +34,6 @@ $string['pluginname_link'] = 'question/type/ddwtos';
 $string['pluginnameadding'] = 'Adding a drag and drop into text';
 $string['pluginnameediting'] = 'Editing a drag and drop into text';
 $string['pluginnamesummary'] = 'Missing words in the question text are filled in using drag and drop.';
+
+$string['ordered'] = 'Ordered';
+$string['ordered_help'] = 'Checking this checkbox this question will consider the answers order for marks. Otherwise, it won\'t';
